4a9254f4a9694bb498ce99820a0a5fbe058de2de
Joaquin <salgadoisai64@gmail.com>
2024-02-13 19:58:00 -0600

    cfre

---
+++ prueba/prueba.py
@@ -0,0 +1,7 @@
+
+print("Hello World!\nHello World!\nHello World!")
+print("Hello " + " " + "Isai")
+
+print("Day 1 - python Print Function")
+print("the function is declared like this:")
+print("print('what to print')")
